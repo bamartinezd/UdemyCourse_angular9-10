e484f6ccee230280b2b67442fe7502e1e56c23e4
Brayan Alejandro Martinez Delgado <alejandromardelg@outlook.com>
2020-08-09 19:47:35 -0500

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsProductComponent } from './pages/details-product/details-product.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'products', component: ProductListComponent},
+  {path:'product/:id', component: DetailsProductComponent},
+  {path:'**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsProductComponent } from './pages/details-product/details-product.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent,
+    DetailsProductComponent,
+    NotFoundComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
